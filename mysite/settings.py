e3aabe6908f7a99ef160df8910357cc2462bfa38
Pierre <54605473+npb28@users.noreply.github.com>
2020-02-05 11:41:34 -0600

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -73,6 +73,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+#Using Postgres database
 
 DATABASES = {
     'default': {
