3ea737ffbd2892078ff1ea4fe6d5ae1d34b57f2c
Pierre <54605473+npb28@users.noreply.github.com>
2020-02-05 12:36:53 -0600

    Update forms.py

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,9 +1,13 @@
+# Allows user to create a new contact and add it to the model
+# Django creates form for me based off the model. 
+
 from django import forms
 
 from .models import Contact
 
 class PostForm(forms.ModelForm):
 
+     # Class Meta tells Django which model to create form for
     class Meta:
         model = Contact
         fields = ('first_name', 'last_name', 'phone_number')
