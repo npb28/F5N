352f01660fe4bb94f6e0fb376781ed6464a57e45
Pierre <54605473+npb28@users.noreply.github.com>
2020-02-05 11:53:43 -0600

    Update urls.py

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
+# urls that point to views
 urlpatterns = [
     path('', views.post_list, name='contact_list'),
     path('contact/<int:pk>/', views.contact_detail, name='contact_detail'),
