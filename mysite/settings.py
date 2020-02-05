d44e21795c0f946b8985274735da91858166df59
Pierre <54605473+npb28@users.noreply.github.com>
2020-02-05 11:43:30 -0600

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contacts.apps.ContactsConfig',
+    'contacts.apps.ContactsConfig', # Tell Django use our app
 ]
 
 MIDDLEWARE = [
