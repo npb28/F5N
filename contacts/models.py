07e1d31410fd18a951679f2fd4b6ea00e24b9ca4
Pierre <54605473+npb28@users.noreply.github.com>
2020-02-05 11:47:32 -0600

    Update models.py

--- contacts/models.py
+++ contacts/models.py
@@ -2,12 +2,13 @@ from django.conf import settings
 from django.db import models
 
 
+# Creat a contact object that holds first/last name and number
 class Contact(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=200)
 
-
+    # Returns contact's last name in string from
     def __str__(self):
         return self.last_name
