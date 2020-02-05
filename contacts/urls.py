dc10717e04c2cd5aab9548e31fba04d436e18dce
Pierre <54605473+npb28@users.noreply.github.com>
2020-02-05 12:26:08 -0600

    Update urls.py

--- contacts/urls.py
+++ contacts/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='contact_list'),
     path('contact/<int:pk>/', views.contact_detail, name='contact_detail'),
+    # Expects int value that it will then transfer to views as a variable called pk
     path('create/new/', views.create_new, name='create_new'),
 ]
